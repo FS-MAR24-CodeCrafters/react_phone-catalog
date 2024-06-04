96ac0c59260a80395086eec66e02ffce581cdeb9
Oleh Dynys <dynys.oleh@gmail.com>
2024-06-04 12:28:49 +0300

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './layout/Header';
 import { Button } from './ui/buttons';
-// import { Dropdown } from './ui/buttons/Dropdown';
 
-// const items = [
-//   { id: 1, label: 'Item 1', value: 'item1' },
-//   { id: 2, label: 'Item 2', value: 'item2' },
-//   { id: 3, label: 'Item 3', value: 'item3' },
-// ];
 const label = 'Button works';
 
 const App = () => {
@@ -19,7 +13,6 @@ const App = () => {
       <main className="container">
         <Outlet />
         <Button label={label} />
-        {/* <Dropdown items={items} /> */}
       </main>
     </div>
   );
