77cdfd5f8b557940e3108900e55f949bf07198b2
Oleh Dynys <dynys.oleh@gmail.com>
2024-06-04 12:13:42 +0300

    add button

--- src/ui/buttons/button.tsx
+++ src/ui/buttons/button.tsx
@@ -1,3 +1,24 @@
-export const button = () => {
-  return <>Button Works</>;
+import React from 'react';
+import './Button.scss';
+
+type Props = {
+  label: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  selected?: boolean;
+};
+
+export const Button: React.FC<Props> = ({
+  label,
+  onClick,
+  disabled,
+  selected,
+}) => {
+  const buttonClasses = `button ${selected ? 'selected' : ''} ${disabled ? 'disabled' : ''}`;
+
+  return (
+    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
+      {label}
+    </button>
+  );
 };
