6d9cd4624c32345f552bf7cd07133dec393b80bf
Grygoryy <kachurovskyy123@gmail.com>
2024-06-04 11:34:50 +0300

    product-card developing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
-import { BurgerMenu } from './components/BurgerMenu';
-import { ProductCard } from './components/ProductCard/ProductCard';
 import { Header } from './layout/Header';
+import { ProductCard } from './components/ProductCard/ProductCard';
 
 const App = () => {
   return (
     <div className="App">
-      <h1
-        className="Person VeryHugeClassName"
-        id="Person"
-        style={{ margin: '0' }}
-      >
-        Product Catalog
-      </h1>
-      <BurgerMenu />
-      <ProductCard />
       <Header />
-
+      <ProductCard />
       <main className="container">
         <Outlet />
       </main>
