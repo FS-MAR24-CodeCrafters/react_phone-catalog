c3d5a678c5a30dd8c6f3bb3e7158cdb0f7f93109
ArtemZakhar <artem.zakharchuk@ukr.net>
2024-06-04 15:54:34 +0300

    fixed linter bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './layout/Header';
-import { Button } from './ui/buttons';
+import { Button } from './ui/Buttons';
 import { ProductCard } from './components/ProductCard/ProductCard';
-        
+
 const label = 'Button works';
 
 const App = () => {
