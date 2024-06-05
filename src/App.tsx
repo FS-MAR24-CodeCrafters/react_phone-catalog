c34cf0be90fe8f046d2b7e201ee8aaab30de7e90
Taras <tarasamchenko@gmail.com>
2024-06-05 21:59:54 +0300

    before merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { Outlet } from 'react-router-dom';
 import './App.scss';
 import { Header } from './layout/Header';
 import { ProductCard } from './components/ProductCard/ProductCard';
+import { CartItem } from './components/CartItem';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <ProductCard />
+      <br />
+      <CartItem />
       <main className="container">
         <Outlet />
       </main>
