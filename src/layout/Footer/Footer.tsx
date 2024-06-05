b107ef7e07a20815afa4f068581d6db11c7b7576
Oleh Tsiolkovskiy <olehtsiolkovskiy86@gmail.com>
2024-06-05 15:35:23 +0300

    Add footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
 import classes from './Footer.module.scss';
+import logo from '../../../public/img/LogoFooter.png';
 
 export function Footer() {
-  return <h1 className={classes.test}>Footer works</h1>;
+  return (
+    <footer className={`${classes.footer}`}>
+      <div>
+        <Link to="/" className={classes.link}>
+          <img src={logo} alt="logo" className={classes.footer__logo} />
+        </Link>
+      </div>
+
+      <div className={classes.footer__info}>
+        <a href="#">Github</a>
+        <a href="#">Contacts</a>
+        <a href="#">Rights</a>
+      </div>
+
+      <div className={classes.footer__button}>
+        <span>Back to top</span>
+        <button className={classes.button_back} aria-label="Back to top" />
+      </div>
+    </footer>
+  );
 }
