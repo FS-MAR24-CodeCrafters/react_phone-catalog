8c14b70d5e5200316396d7e692f757cbc477f412
ArtemZakhar <artem.zakharchuk@ukr.net>
2024-06-05 11:58:24 +0300

    arrows and numbers buttons have to be finnised

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { useContext, useEffect } from 'react';
 import { Header } from './layout/Header';
-import { Button } from './ui/Buttons';
-import { ProductCard } from './components/ProductCard/ProductCard';
-
-const label = 'Button works';
+import { Arrow } from './ui/Arrow/Arrow';
+import { arrowDir } from './types/arrowEnum';
+import { PhoneDispatchContext } from './store/phoneStore/phoneContext';
+import { ActionsName } from './types/phones/phoneActions';
 
 const App = () => {
+  const dispatch = useContext(PhoneDispatchContext);
+
+  useEffect(() => {
+    fetch('../public/api/phones.json')
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({ type: ActionsName.initialAction, payload: res });
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <ProductCard />
+      <Arrow dir={arrowDir.down} />
       <main className="container">
         <Outlet />
-        <Button label={label} />
       </main>
     </div>
   );
