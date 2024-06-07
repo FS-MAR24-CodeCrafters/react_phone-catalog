14119a61cbe7c5b029eb39f16816f3a7758e190b
Grygoryy <kachurovskyy123@gmail.com>
2024-06-07 13:44:13 +0300

    catalog-finished

--- src/components/CatalogPage/CatalogList.tsx
+++ src/components/CatalogPage/CatalogList.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import classes from './CatalogList.module.scss';
 import { ProductCard } from '../ProductCard';
@@ -8,16 +9,23 @@ import { Product } from '../../types/product';
 export const CatalogList = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
+  const { pathname } = useLocation();
+  const path = pathname.slice(1);
+
+  const filteredProducts = products.filter((product) => {
+    return product.category === path;
+  });
+
   useEffect(() => {
     getGoods<Product[]>('products.json').then((res) => {
-      setProducts(res.filter((_item, index) => index <= 16));
+      setProducts(res);
     });
   }, []);
 
   return (
     <div className={classes.catalog__container}>
       <div className={classes.catalog__main}>
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <div className={classes.catalog__item} key={product.id}>
             <ProductCard product={product} />
           </div>
