5aeb38dff0ca09493a73907a2e6cbd776bf768bb
ArtemZakhar <artem.zakharchuk@ukr.net>
2024-06-11 12:01:20 +0300

    before merge

--- src/constants/sliders.ts
+++ src/constants/sliders.ts
@@ -1,6 +1,26 @@
 export const sliders = [
   {
     id: 1,
-    src: 'src/img/banner-main.svg',
+    src: './img/main-slider/banner-phone.png',
+    link: 'phones',
+    background: '#201D24',
+  },
+  {
+    id: 2,
+    src: './img/main-slider/banner-phones.png',
+    link: 'phones',
+    background: '#FBF7F4',
+  },
+  {
+    id: 3,
+    src: './img/main-slider/banner-tablets.png',
+    link: 'tablets',
+    background: '#faf7f2',
+  },
+  {
+    id: 4,
+    src: './img/main-slider/banner-accessories.png',
+    link: 'accessories',
+    background: '#6ba1c4',
   },
 ];
--- src/pages/ItemCardPage/ItemCardPage.tsx
+++ src/pages/ItemCardPage/ItemCardPage.tsx
@@ -1,33 +1,3 @@
-import { useEffect } from 'react';
-import { getGoods } from '../../api/goods';
-import { Gadget } from '../../types/gadget';
-
 export const ItemCardPage = () => {
-  useEffect(() => {
-    const phones = getGoods<Gadget[]>('phones.json');
-    const tablets = getGoods<Gadget[]>('tablets.json');
-    const accessories = getGoods<Gadget[]>('accessories.json');
-
-    Promise.all([phones, tablets, accessories]).then((values) => {
-      const colors = new Set<string>();
-
-      values.forEach((value) => {
-        value.forEach((item) => {
-          colors.add(item.color);
-        });
-      });
-
-      const arrColor = [...colors];
-
-      const colorsObj = {};
-
-      arrColor.forEach((color) => {
-        colorsObj[color] = '';
-      });
-
-      window.console.log(colorsObj);
-    });
-  }, []);
-
   return <h1>ItemCard works</h1>;
 };
