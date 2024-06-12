ff9830d7cd6a85669c73e399e199e689cfb68a0c
Grygoryy <kachurovskyy123@gmail.com>
2024-06-12 18:40:24 +0300

    before merge

--- src/components/FavoritesPage/FavoritesList.tsx
+++ src/components/FavoritesPage/FavoritesList.tsx
@@ -1,24 +1,20 @@
+import React from 'react';
 import classes from './FavoriteList.module.scss';
-
-import { PageHeader } from '../PageHeader';
 import { ProductCard } from '../ProductCard';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { Product } from '../../types/product';
 
-export const FavoritesList = () => {
-  const [products] = useLocalStorage();
+type FavoriteListProps = {
+  products: Product[];
+}
 
+export const FavoritesList: React.FC<FavoriteListProps> = ({ products }) => {
   return (
-    <div className={classes.favorites__container}>
-      <div className={classes.favorites__header}>
-        <PageHeader title="Favourites" totalModels={products.length} />
-      </div>
-      <div className={classes.favorites__grid}>
-        {products.map((product) => (
-          <div className={classes.favorites__grid_item} key={product.id}>
-            <ProductCard product={product} key={product.id} />
-          </div>
-        ))}
-      </div>
+    <div className={classes.favorites__grid}>
+      {products.map((product) => (
+        <div className={classes.favorites__grid_item} key={product.id}>
+          <ProductCard product={product} key={product.id} />
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/FavouritesPage/FavouritesPage.tsx
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -1,5 +1,19 @@
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { FavoritesList } from '../../components/FavoritesPage';
+import { PageHeader } from '../../components/PageHeader';
+import classes from './FavouritesPage.module.scss';
 
 export const FavouritesPage = () => {
-  return <FavoritesList />;
+  const [products] = useLocalStorage();
+
+  return (
+    <>
+      <div className={classes.favorites__container}>
+        <div className={classes.favorites__header}>
+          <PageHeader title="Favourites" totalModels={products.length} />
+        </div>
+      </div>
+      <FavoritesList products={products} />
+    </>
+  );
 };
